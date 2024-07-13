5e2397f6f44f4ccdd835992b6fb7cb50268df95d
Oleh Ocheretko <117608037+Ocheretko1992@users.noreply.github.com>
2024-07-13 06:31:01 +0300

    finish

---
+++ src/components/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import css from "./App.module.css";
+
+export default function App() {
+  const [initialDate, setInitialDate] = useState(
+    new Date().toISOString().slice(0, 16)
+  );
+  const [hoursToAdd, setHoursToAdd] = useState();
+  const [newDate, setNewDate] = useState("");
+
+  const addHoursToDate = (date, hours) => {
+    const result = new Date(date);
+    result.setHours(result.getHours() + hours);
+    return result;
+  };
+
+  const handleAddHours = () => {
+    const result = addHoursToDate(initialDate, hoursToAdd);
+
+    // Форматуємо дату на українську мову
+    const formattedDate = result.toLocaleString("uk-UA", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+    });
+
+    setNewDate(formattedDate);
+  };
+
+  return (
+    <div className={css.containerApp}>
+      <div className={css.app}>
+        <h1 className={css.appH1}>Додавання годин до Дати</h1>
+        <label className={css.labelPosition}>
+          <span className={css.appH2}>Початкова дата і час</span>
+          <input
+            className={css.dataApp}
+            type="datetime-local"
+            value={initialDate}
+            onChange={(e) => setInitialDate(e.target.value)}
+          />
+        </label>
+        <label className={css.sumAppHours}>
+          <span className={css.appH3}>Кількість годин</span>
+          <input
+            className={css.dataApp2}
+            type="number"
+            value={hoursToAdd}
+            onChange={(e) => setHoursToAdd(parseInt(e.target.value))}
+          />
+        </label>
+        <button className={css.addClock} onClick={handleAddHours}>
+          Додати години
+        </button>
+        {newDate && (
+          <div>
+            <h2>Нова дата і час</h2>
+            <p className={css.appNewDate}>{newDate}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
